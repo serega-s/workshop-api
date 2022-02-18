594da9de9bbddfc3267bf3d7496bcf6a865d3479
serega-s <comp.it.05@inbox.ru>
2022-02-18 16:45:29 +0300

    cors middleware

---
+++ README.md
@@ -0,0 +1,15 @@
+# Workshop API
+
+Workshop API is a backend of service for accounting personal incomes and outcomes.
+
+### Run server
+
+```
+python -m workshop
+```
+
+### API Docs Route
+
+```
+http://127.0.0.1:8000/api/docs
+```
--- workshop/app.py
+++ workshop/app.py
@@ -1,8 +1,9 @@
 from fastapi import FastAPI
-
-from workshop.tables import Base
+from fastapi.middleware.cors import CORSMiddleware
 
 from .api import router
+from .database import engine
+from .tables import Base
 
 tags_metadata = [
     {
@@ -19,10 +20,25 @@ tags_metadata = [
     }
 ]
 
+Base.metadata.create_all(bind=engine)
+
+
 app = FastAPI(
     title='Workshop API',
     description='Workshop API is a backend of service for accounting personal incomes and outcomes',
     version='1.0.0',
     openapi_tags=tags_metadata
 )
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
 app.include_router(router)
